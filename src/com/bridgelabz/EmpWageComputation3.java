7bb0fe943b49923051700f546697ed731502e06d
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-09-17 21:53:37 +0530

    Third UC Case

---
+++ src/com/bridgelabz/EmpWageComputation3.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class EmpWageComputation3 {
+    public static void main(String[] args) {
+
+        int Emp_RatePerHrs = 20;
+        int emphrs = 0;
+        int empwage = 0;
+        int CheckEmp = (int) Math.floor(Math.random() * 10 % 3);
+
+        if (CheckEmp == 1) {
+            System.out.println("Employee is present full-time");
+            emphrs = 8;
+        } else if (CheckEmp == 2) {
+            System.out.println("Employee is present part-time");
+            emphrs = 4;
+        } else {
+            System.out.println("Employee is absent");
+            emphrs = 0;
+        }
+
+        empwage = emphrs * Emp_RatePerHrs;
+        System.out.println("Employee Wage is : " + empwage);
+    }
+}
