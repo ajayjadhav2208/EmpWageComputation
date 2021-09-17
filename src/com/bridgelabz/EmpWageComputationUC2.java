b1f2b0c437f67df73fe2351cd6424a50c77c57e0
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-09-17 21:45:14 +0530

    Second UC Case

---
+++ src/com/bridgelabz/EmpWageComputationUC2.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class EmpWageComputationUC2 {
+
+    public static void main(String[] args) {
+        int Emp_RatePerHrs = 20;
+        int emphrs = 0;
+        int empwage = 0;
+        int CheckEmp = (int) Math.floor(Math.random() * 10 % 2);
+
+        if (CheckEmp == 1) {
+            emphrs = 8;
+        } else {
+            emphrs = 0;
+        }
+
+        empwage = emphrs * Emp_RatePerHrs;
+        System.out.println("Employee Wage is : " + empwage);
+    }
+}
